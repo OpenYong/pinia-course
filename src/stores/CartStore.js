bf6bdbaafa498453a57f3317a16704d6be9fd9b5
OpenYong <fkszm3@gmail.com>
2023-04-04 22:59:11 +0900

    Challenge: Cart Modifications, Setting Item Counts in the Cart

--- src/stores/CartStore.js
+++ src/stores/CartStore.js
@@ -10,8 +10,15 @@ export const useCartStore = defineStore("CartStore", {
   getters: {
     count: (state) => state.items.length,
     isEmpty: (state) => state.count === 0,
-    grouped: (state) => groupBy(state.items, (item) => item.name),
+    grouped: (state) => {
+      const grouped = groupBy(state.items, (item) => item.name);
+      const sorted = Object.keys(grouped).sort();
+      let inOrder = {};
+      sorted.forEach((key) => (inOrder[key] = grouped[key]));
+      return inOrder;
+    },
     groupedCount: (state) => (name) => state.grouped[name].length,
+    total: (state) => state.items.reduce((acc, item) => acc + item.price, 0),
   },
   actions: {
     addItems(count, item) {
@@ -19,6 +26,13 @@ export const useCartStore = defineStore("CartStore", {
       for (let index = 0; index < count; index++) {
         this.items.push({ ...item });
       }
+    },
+    clearItem(name) {
+      this.items = this.items.filter((item) => item.name !== name);
+    },
+    setItemCount(item, count) {
+      this.clearItem(item.name);
+      this.addItems(count, item);
     },
   },
 });
