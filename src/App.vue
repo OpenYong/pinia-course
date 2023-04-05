96e8da3709a11b1f7c0b0d55c1d9ef542bf6378c
OpenYong <fkszm3@gmail.com>
2023-04-05 10:38:15 +0900

    Subscribing to States

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref, reactive } from "vue";
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "./stores/ProductStore";
@@ -6,6 +7,22 @@ import { useCartStore } from "./stores/CartStore";
 
 const productStore = useProductStore();
 const cartStore = useCartStore();
+const history = reactive([]); // store - state의 변화를 기록하기 위한 배열
+const doingHistory = ref(false);
+
+history.push(JSON.stringify(cartStore.$state)); // $state는 모든 state에 접근하기 위함
+const undo = () => {
+  if (history.length === 1) return; // 초기값이기 때문에 더이상 undo할 수 없음
+  doingHistory.value = true;
+  history.pop();
+  cartStore.$state = JSON.parse(history.at(-1));
+  doingHistory.value = false;
+};
+cartStore.$subscribe((mutation, state) => {
+  if (!doingHistory.value) {
+    history.push(JSON.stringify(state));
+  }
+});
 cartStore.$onAction(({ name, store, args, after, onError }) => {
   if (name === "addItems") {
     after(() => {
@@ -27,6 +44,9 @@ productStore.fill();
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">취소하기</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
